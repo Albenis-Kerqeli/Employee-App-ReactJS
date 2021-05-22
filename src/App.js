659bfebebb3c3a38d5613948979b4cf1b507e8e8
Albenoo <albeniskerqeli5@gmail.com>
2021-05-22 02:11:17 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './style.css';
 import React, { useState } from 'react';
 import AppForm from './Components/AppForm';
+import AppTable from './Components/AppTable';
+
 export default function App() {
   return (
     <div className="App">
       <div className="App__row" />
       <AppForm />
+      <AppTable />
     </div>
   );
 }
